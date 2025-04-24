3c95d55750c7bcb2a5454cfa058d37ddd1ce8e0f
jashwanrh79 <jashwanthpatlo@gmail.com>
2025-04-24 15:16:09 +0530

    Add files via upload

---
+++ marge.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];//sub array
+
+    for (int i = 0; i < n1; i++)//copy of elements into temp array
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;//comparing elements and copying in main array
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    //copying any leftover elemens
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int i,a[100],n;
+    printf("entr no.of ele");
+    scanf("%d",&n);
+    printf("enter %d ele ",n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("Given array: ");
+    printArray(a,n);
+
+    mergeSort(a, 0,n - 1);
+
+    printf("Sorted array: ");
+    printArray(a,n);
+
+    return 0;
+}
